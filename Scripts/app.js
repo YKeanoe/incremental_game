1635948450308202f74f2dcb497a7fface0950f5
Yohanes Keanoe <21034098+ZankiMaru@users.noreply.github.com>
2017-11-08 22:46:24 +1100

    Model development

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,9 +2,8 @@ function fillCanvas(){
     var canvas = document.getElementById('canvas');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
-        ctx.fillStyle = "blue";
+        ctx.fillStyle = 'rbg(200,200,200)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);    
-        
     }
 }
 
@@ -25,48 +24,54 @@ function draw(num) {
 
         var rowY = 10;
         var rowYOffset = rowY * 2;
-
+        
+        /*
+        if(num==1){
+            // first triangle
+            drawTriangle(ctx, triangleSideLength, offset1, 0);
+        } else 
+        */
         if(num<=9){
             // Top first part of hex
             if(num%2==0){
-                drawTriangleInvert(ctx, triangleSideLength,(num*TriangleHalfSideLength)+offset1,rowY);            
+                drawTriangleInvert(ctx, triangleSideLength,(num*TriangleHalfSideLength)+offset1,0);            
             }else{
-                drawTriangle(ctx, triangleSideLength,(num*TriangleHalfSideLength)+offset1,rowY);
+                drawTriangle(ctx, triangleSideLength,(num*TriangleHalfSideLength)+offset1,0);
             }
         } else if(num<=20){
             // Top second part of hex
             if(num%2!=0){
-                drawTriangleInvert(ctx, triangleSideLength,((num-9)*TriangleHalfSideLength)+offset2,(rowY*2));            
+                drawTriangleInvert(ctx, triangleSideLength,((num-9)*TriangleHalfSideLength)+offset2,(rowY*1));            
             }else{
-                drawTriangle(ctx, triangleSideLength,((num-9)*TriangleHalfSideLength)+offset2,(rowY*2));
+                drawTriangle(ctx, triangleSideLength,((num-9)*TriangleHalfSideLength)+offset2,(rowY*1));
             }
         } else if(num<=33){
             // Top third part of hex
             if(num%2==0){
-                drawTriangleInvert(ctx, triangleSideLength,(num-20)*TriangleHalfSideLength,(rowY*3));            
+                drawTriangleInvert(ctx, triangleSideLength,(num-20)*TriangleHalfSideLength,(rowY*2));            
             }else{
-                drawTriangle(ctx, triangleSideLength,(num-20)*TriangleHalfSideLength,(rowY*3));
+                drawTriangle(ctx, triangleSideLength,(num-20)*TriangleHalfSideLength,(rowY*2));
             }
         } else if(num<=46){
             // Middle bottom part of hex
             if(num%2!=0){
-                drawTriangle(ctx, triangleSideLength,(num-33)*TriangleHalfSideLength,(rowY*4));
+                drawTriangle(ctx, triangleSideLength,(num-33)*TriangleHalfSideLength,(rowY*3));
             }else{
-                drawTriangleInvert(ctx, triangleSideLength,(num-33)*TriangleHalfSideLength,(rowY*4));            
+                drawTriangleInvert(ctx, triangleSideLength,(num-33)*TriangleHalfSideLength,(rowY*3));            
             }
         } else if(num<=57){
             // Middle bottom part of hex
             if(num%2==0){
-                drawTriangle(ctx, triangleSideLength,((num-46)*TriangleHalfSideLength)+offset2,(rowY*5));
+                drawTriangle(ctx, triangleSideLength,((num-46)*TriangleHalfSideLength)+offset2,(rowY*4));
             }else{
-                drawTriangleInvert(ctx, triangleSideLength,((num-46)*TriangleHalfSideLength)+offset2,(rowY*5));            
+                drawTriangleInvert(ctx, triangleSideLength,((num-46)*TriangleHalfSideLength)+offset2,(rowY*4));            
             }
         } else{
             // Bottom part of hex
             if(num%2!=0){
-                drawTriangle(ctx, triangleSideLength,((num-57)*TriangleHalfSideLength)+offset1,(rowY*6));
+                drawTriangle(ctx, triangleSideLength,((num-57)*TriangleHalfSideLength)+offset1,(rowY*5));
             }else{
-                drawTriangleInvert(ctx, triangleSideLength,((num-57)*TriangleHalfSideLength)+offset1,(rowY*6));            
+                drawTriangleInvert(ctx, triangleSideLength,((num-57)*TriangleHalfSideLength)+offset1,(rowY*5)); 
             }
         }
     }
@@ -81,7 +86,7 @@ function drawTriangle(ctx, side, x, y){
     ctx.lineTo(x+side, y+side);
     // line to top
     ctx.lineTo(x+(side/2),y);
-    ctx.fillStyle="red";
+    ctx.fillStyle="white";
     ctx.fill();
     ctx.closePath();
 }
@@ -95,7 +100,7 @@ function drawTriangleInvert(ctx, side, x, y){
     ctx.lineTo(x+side, y);
     // line to bottom
     ctx.lineTo(x+(side/2),y+side);
-    ctx.fillStyle="green";
+    ctx.fillStyle="white";
     ctx.fill();
     ctx.closePath();
 }
@@ -103,6 +108,7 @@ function drawTriangleInvert(ctx, side, x, y){
 var app = angular.module("myApp", ['ngRoute', 'ngResource']);
 app.factory('userService', ['$rootScope', function ($rootScope) {
         var service = {
+            // Store how much points user have
             model: {
                 sim: 0,
                 house : 0,
@@ -115,6 +121,40 @@ app.factory('userService', ['$rootScope', function ($rootScope) {
                 spnation : 0
             },
     
+            // Store how much (base) points is added per second
+            basePointsPerSecond: {
+                sim: 1,
+                house : 0,
+                hamlet : 0,
+                village : 0,
+                town : 0,
+                city : 0,
+                kingdom : 0,
+                empire : 0,
+                spnation : 0
+            },
+
+            // Store modifier for points per second
+            modifier: {
+                sim: 1,
+                house : 0,
+                hamlet : 0,
+                village : 0,
+                town : 0,
+                city : 0,
+                kingdom : 0,
+                empire : 0,
+                spnation : 0
+            },
+
+            UpdateModel: function(){
+                console.log(service.model.sim);
+
+                service.model.sim += service.basePointsPerSecond.sim * service.modifier.sim;
+
+                console.log(service.model.sim);
+            },
+
             SaveState: function () {
                 sessionStorage.userService = angular.toJson(service.model);
             },
@@ -129,36 +169,40 @@ app.factory('userService', ['$rootScope', function ($rootScope) {
         return service;
     }]);
     
-app.controller('appController', function(userService, $scope, $interval) {
+app.controller('houseController', function(userService, $scope, $interval, $location) {
     $scope.sim = userService.model.sim;
-    $scope.house = 0;
-    $scope.hamlet = 0;
-    $scope.village = 0;
-    $scope.town = 0;
-    $scope.city = 0;
-    $scope.kingdom = 0;
-    $scope.empire = 0;
-    $scope.spnation = 0;
+    $scope.house = userService.model.house;
+    $scope.hamlet = userService.model.hamlet;
+    $scope.village = userService.model.village;
+    $scope.town = userService.model.town;
+    $scope.city = userService.model.city;
+    $scope.kingdom = userService.model.kingdom;
+    $scope.empire = userService.model.empire;
+    $scope.spnation = userService.model.spnation;
+
     $scope.$on('$routeChangeSuccess', function() {
         fillCanvas();
     });
     
     $interval(function () {
-        $scope.sim ++;
-        userService.model.sim = $scope.sim;  
         draw($scope.sim);
-    }, 500);
+    }, 10);
 
+    $interval(function () {
+        userService.UpdateModel();
+        $scope.sim = userService.model.sim;        
+    }, 1000);
 });
+
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
     .when("/", {
-        controller : 'appController',
+        controller : 'houseController',
         templateUrl : 'Pages/house.html'
     })
     .when("/hamlet", {
-        controller : 'appController',        
+        controller : 'houseController',        
         templateUrl : 'Pages/hamlet.html'
     })
             
