586bf9543d6ce92b0796ebe0bb4d78e2c4034d65
Yohanes Keanoe <21034098+ZankiMaru@users.noreply.github.com>
2017-11-27 23:46:22 +1100

    tt

--- Scripts/test.spec.js
+++ Scripts/test.spec.js
@@ -1,5 +1,5 @@
 // require('/Vendors/jquery/jquery-3.2.1.min.js');
-// var angular = require('/Vendors/angularjs-1.6.6/angular.min.js');
+var angular = require('/Vendors/angularjs-1.6.6/angular.min.js');
 // require('/Vendors/angularjs-1.6.6/angular-route.min.js');
 // require('/Vendors/angularjs-1.6.6/angular-resource.min.js');
 // require('/Vendors/angularjs-1.6.6/angular-mocks.js');
@@ -16,7 +16,7 @@ describe('Main Controller Test', function () {
 
     beforeEach(function(){
         // Load Module
-        module('myApp');
+        angular.module('myApp');
 
         // Inject services and dependencies
         inject(function(_$controller_, _$rootScope_, $injector, _$interval_, _$location_, _$route_){
