656fe9bd442a72b726067ab9dd9c7c1c64fcd222
Yohanes Keanoe <21034098+ZankiMaru@users.noreply.github.com>
2017-11-28 21:08:45 +1100

    karma test

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+        'src/*.js',
+        'spec/*.spec.js'
+        ],
+        exclude: [
+        ],
+        preprocessors: {},
+        reporters: ['dots'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['PhantomJS'],
+        singleRun: true
+    });
+};
+  
