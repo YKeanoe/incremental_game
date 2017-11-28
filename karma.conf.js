eafa8beb984431f917bffa9e88877aa3678bac49
Yohanes Keanoe <21034098+ZankiMaru@users.noreply.github.com>
2017-11-28 19:36:18 +1100

    test

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,8 @@
 
 module.exports = function (config) {
     config.set({
-        basePath: './',
-        singleRun: true,
-        autoWatch: false,
+        basePath: '',
         frameworks: ['jasmine'],
-        browsers: ['PhantomJS'],
         plugins: [
             'karma-coverage',
             'karma-phantomjs-launcher',
@@ -14,7 +11,10 @@ module.exports = function (config) {
         ],
         files: [
             'Vendors/angularjs-1.6.6/*.js',
-            'src/*.js'
-        ]
+            'src/*.js',
+            'tests/*.spec.js'
+        ],
+        reporters: ['progress']
+        
     });
 };
